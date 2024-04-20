94318c7fc590f95948693d8d13316fdace378ad8
Jaejin Song <songjj77@naver.com>
2024-04-20 16:54:06 +0900

    docs: input storybook

---
+++ packages/components/src/components/input/stories/Mask.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const value = ref('');
+const value2 = ref('');
+</script>
+<template>
+  <div>
+    <n-input v-model="value" mask="###-###"></n-input>
+    <div>
+      {{ `value: ${value}` }}
+    </div>
+  </div>
+  <div>
+    <n-input v-model="value2" mask="###-###" fill-mask></n-input>
+    <div>
+      {{ `value: ${value}` }}
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped></style>
